5bdc06e0156c69d3c19abd9dc3224501ac76ad44
kanumalivad <kanu.malivad@gmail.com>
2019-12-15 18:17:24 +0530

    Add antd design

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+import { Badge, Button, Icon } from 'antd';
+
+const Content = Layout.Content;
+const ButtonGroup = Button.Group;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        My App
-      </div>
+     <Layout>
+       <Content>
+        <Badge count={1} style={{ backgroundColor: '#52c41a',  }} />
+       </Content>
+       <ButtonGroup>
+          <Button onClick={this.decline}>
+            <Icon type="minus" />
+          </Button>
+          <Button onClick={this.increase}>
+            <Icon type="plus" />
+          </Button>
+        </ButtonGroup>
+     </Layout>
     );
   }
 }
